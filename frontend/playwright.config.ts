05c2dc3ff592f61ebdfd09dff3b5c23f873017c8
Mark McDermott <mark@markmcdermott.io>
2024-10-05 00:19:47 -0500

    Adding playwright

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./spec/e2e",
   outputDir: "./spec/e2e/videos",
-  use: { video: "on", baseURL: "http://localhost:3001" },
+  use: { video: "on", baseURL: process.env.BASE_URL || 'http://localhost:3000' },
   projects: [
     { name: "chromium", use: { ...devices["Desktop Chrome"] } },
     { name: "firefox", use: { ...devices["Desktop Firefox"] } },
